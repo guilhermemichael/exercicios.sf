559e60d3fa735592766090fff147129ce960da41
Guilherme Michael <guilherme.michael.dev@gmail.com>
2026-03-22 00:55:06 -0300

    Add script to calculate student average grade

---
+++ Exercício_1.py
@@ -0,0 +1,11 @@
+#1. Média Aritmética: Peça três notas de um aluno e calcule a média final.
+
+print("Faça sua Média Aritmética")
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+
+mf = (n1 + n2 + n3) / 3
+
+print(f"A média final é: {mf:.2f}")
