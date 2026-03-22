aa99672ee6337be8dc2d3e4750ce3ec265e39a93
Guilherme Michael <guilherme.michael.dev@gmail.com>
2026-03-22 01:01:43 -0300

    Add program to calculate double and triple of a number
    Implement a program to calculate and display the double and triple of a number input by the user.

---
+++ Exercício_4.py
@@ -0,0 +1,9 @@
+# 4. Dobro e Triplo: Leia um número e mostre o seu dobro e o seu triplo.
+
+n1 = float(input("Digite o número a qual deseja calcular o dobro e o triplo: "))
+
+db = n1 * 2
+
+print(f"\nNúmero: {n1}")
+print(f"Dobro: {db}")
+print(f"Triplo: {n1 * 3}")
