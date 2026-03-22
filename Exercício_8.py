6d10fc4ab9949c1ad67652d3738b1d68da8fa6b1
Guilherme Michael <guilherme.michael.dev@gmail.com>
2026-03-22 01:03:45 -0300

    Add age to days lived calculator
    Implement a calculator that converts age in years to approximate days lived.

---
+++ Exercício_8.py
@@ -0,0 +1,9 @@
+#8. Calculadora de Dias: Peça a idade de uma pessoa apenas em anos e exiba aproximadamente quantos dias ela já viveu (considere anos de 365 dias).
+
+print("Calculadora de Dias")
+
+a = int(input("Digite a idade de uma pessoa em anos: "))
+d = a * 365
+
+print(f"\nIdade: {a} anos")
+print(f"\nJá viveu aproximadamente {d} dias")
