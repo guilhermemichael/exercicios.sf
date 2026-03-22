fe2288bffbffa25cb0361d961fbb20daaa188e4f
Guilherme Michael <guilherme.michael.dev@gmail.com>
2026-03-22 01:02:22 -0300

    Add wall area calculation script

---
+++ Exercício_5.py
@@ -0,0 +1,10 @@
+# 5. Cálculo de Área: Peça a largura e a altura de uma parede em metros. Calcule a sua área (A = L * H).
+
+print("Cálculo de Área")
+
+l = float(input("Digite a largura de uma parede em metros: "))
+h = float(input("Digite a altura de uma parede em metros: "))
+
+a = l * h
+
+print(f"Área: {a:.2f} m²")
