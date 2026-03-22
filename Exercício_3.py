1889bba534dd683f0839696088e78c61b0c83139
Guilherme Michael <guilherme.michael.dev@gmail.com>
2026-03-22 00:57:18 -0300

    Create Exercício_3.py

---
+++ Exercício_3.py
@@ -0,0 +1,9 @@
+# 3. Sucessor e Antecessor: Leia um número inteiro e mostre na tela o seu antecessor e o seu sucessor.
+
+print("Sucessor e Antecessor")
+
+n1 = int(input("Digite um número inteiro para saber seu antecessor e sucessor: "))
+
+print(f"\nNúmero: {n1}")
+print(f"Antecessor: {n1 - 1}")
+print(f"Sucessor: {n1 + 1}")
